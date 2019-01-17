0b75e76a1b1bc2a3e3b72a2cd0ed0d3698fef8c8
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-01-17 17:23:52 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,7 +1,11 @@
 
 def levitation_quiz
-	puts "What is the spell that enacts levitation?"
+  loop do
+  puts "What is the spell that enacts levitation?"
   answer = gets.chomp
   break if answer == "Wingardium Leviosa"
     puts "You passed the quiz!"
+  end
 end
+
+
